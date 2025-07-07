8fe508534bb6f08a147dc215fae53105e844399f
student-tapu <152904868+student-tapu@users.noreply.github.com>
2025-07-07 19:14:47 +0530

    Update palindromme.java

--- palindromme_string.java
+++ palindromme_string.java
@@ -1,27 +1,23 @@
-import java.util.*;
-class palindromme_string{
-static char name='t';
-int dob=25;
-static int m=10;
-//int op=3;
-    static void get(){
-        System.out.println(name);
-    
+import java.util.Scanner;
 
-    }
-    void let(){
-        System.out.println(dob);
-        int j=3;
-        System.out.println(j+m);
-        j++;
-        m++;
-    }
-    
-    public static void main(String[] args) {
-        
-        palindromme_string s=new palindromme_string();
-        s.let();
-        s.let();
-        System.out.println(args[1]);
-    }
+class palindromme{
+	public static void main(String args[])	{
+		int s=0,r=1;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter number: ");
+		int q=sc.nextInt();
+		int k=q;
+		while(q!=0){
+			r=q%10;
+			s=s*10+r;
+			q=q/10;
+		}
+		if(s==k){
+			System.out.print("palindromme number");
+		}
+		else{
+			System.out.print("not a palindromme number");
+		}
+	}
 }
+
